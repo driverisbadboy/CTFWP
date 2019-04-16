f6e55e92a9484b179ee7dee17e5920c37bd6893b
Yunen <2865859175@qq.com>
2019-04-16 22:49:25 +0800

    更换统计插件

---
+++ _gitbook_plugin_analytics.js
@@ -0,0 +1,9 @@
+if ('2b55d8e2dd73ae8a37978eec7e143a7c') {
+	var _hmt = _hmt || [];
+	(function() {
+	  var hm = document.createElement("script");
+	  hm.src = "https://hm.baidu.com/hm.js?" + '2b55d8e2dd73ae8a37978eec7e143a7c';
+	  var s = document.getElementsByTagName("script")[0];
+	  s.parentNode.insertBefore(hm, s);
+	})();
+}
