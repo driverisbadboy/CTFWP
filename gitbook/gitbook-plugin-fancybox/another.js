66ede6ad5d05d08159f1984c934fb94db3fefa07
Yunen <2865859175@qq.com>
2019-06-24 22:13:53 +0800

    fix bug

--- gitbook/gitbook-plugin-fancybox/another.js
+++ gitbook/gitbook-plugin-fancybox/another.js
@@ -8,3 +8,20 @@ if (wig<800) {
 	$("#anchor-navigation-ex-navbar").css({"right":"50px"});
 	$(".page-inner").css({"padding":"20px 20px 20px 20px"});
 };
+
+require(['gitbook'], function(gitbook, $) {
+	gitbook.events.on('page.change', function() {
+		jQuery(document).ready(function ($) {
+			var pathname = window.location.pathname;if(pathname.substr(0,10) != '/articals/' && pathname.substr(0,11) != '/reply.html'){$("div").removeClass("comment");}
+			$(".gitbook-link").remove();
+			var wig = $(window).width();
+			if (wig<800) {
+				$(".book-anchor").remove();
+				$("#book-search-input").remove();
+				$(".header-nav").remove();
+				$("#anchor-navigation-ex-navbar").css({"right":"50px"});
+				$(".page-inner").css({"padding":"20px 20px 20px 20px"});
+			};
+		});
+    });
+});
